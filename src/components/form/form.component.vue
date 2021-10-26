7f711e83b03d4ed2dc2eb7a205b5ee132ea963b9
szymon.gus <szymon.gus@antologic.com>
2021-10-26 15:15:52 +0200

    Form wrapper component

---
+++ src/components/form/form.component.vue
@@ -0,0 +1,37 @@
+<template>
+  <div/>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'CForm',
+  props: {
+    price: {
+      type: Number,
+      required: true,
+    },
+    rate: {
+      type: Number,
+      required: true,
+    },
+    numberOfRatings: {
+      type: Number,
+      required: true,
+    },
+    availableDates: {
+      type: Array,
+      default: () => [],
+    },
+    selectedDates: {
+      type: Array,
+      default: () => [],
+    }
+  }
+});
+</script>
+
+<style scoped>
+
+</style>
