df8eea1a675761126064bd05184bf5943974a045
szymon.gus <szymon.gus@antologic.com>
2021-10-28 21:23:48 +0200

    Hook correction

--- src/components/calendar/hooks/use-calendar.js
+++ src/components/calendar/hooks/use-calendar.js
@@ -1,14 +1,16 @@
+import { ref }      from 'vue';
 import * as datefns from 'date-fns';
 import { range }    from '@/helpers/helpers';
-import { ref }      from 'vue';
 
 export const PREVIOUS = 'PREVIOUS';
 export const NEXT = 'NEXT';
 export const CURRENT = 'CURRENT';
 export const shortWeekNames = [ 'SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT' ];
-export const monthNames = ['JANUARY', 'FEBRUARY', 'MARCH', 'APRIL', 'MAY', 'JUNE', 'JULY', 'AUGUST', 'SEPTEMBER', 'OCTOBER', 'NOVEMBER', 'DECEMBER']
+
+const monthNames = [ 'JANUARY', 'FEBRUARY', 'MARCH', 'APRIL', 'MAY', 'JUNE', 'JULY', 'AUGUST', 'SEPTEMBER', 'OCTOBER', 'NOVEMBER', 'DECEMBER' ];
+
 export const useCalendar = () => {
-  const date = ref(new Date())
+  const date = ref(new Date());
   const monthDays = ref([]);
   const currentMonthAndYear = ref('');
 
@@ -17,14 +19,17 @@ export const useCalendar = () => {
     const currentYear = date.value.getFullYear();
     const numberOfDaysInCurrentMonth = datefns.getDaysInMonth(date.value);
 
-    currentMonthAndYear.value = `${monthNames[currentMonth]} ${currentYear}`
+    currentMonthAndYear.value = `${monthNames[ currentMonth ]} ${currentYear}`;
 
+    // Months are from 0 - 11 there we check if previous is December;
     const prevMonth = currentMonth - 1 < 0 ? 11 : currentMonth - 1;
     const daysInPrevMonth = datefns.getDaysInMonth(new Date(prevMonth === 11 ? currentYear - 1 : currentYear, prevMonth, 1));
 
+    //numbers representing day of week 0 - Sunday etc.
     const firstDayOfMonth = datefns.getDay(new Date(currentYear, currentMonth, 1));
     const lastDayOfMonth = datefns.getDay(datefns.lastDayOfMonth(date.value));
 
+    //First day of month is a number from 0 - 6 - we need to get 0 to 6 days from previous month
     const getPrevMonthLastDays = range(firstDayOfMonth).map(index => {
       return {
         value: daysInPrevMonth - index,
@@ -32,36 +37,55 @@ export const useCalendar = () => {
       };
     }).reverse();
 
-    // const daysInNextMonth = datefns.getDaysInMonth(new Date(nextMonth === 0 ? currentYear + 1 : currentYear, nextMonth, 1));
-    const getNextMonthFirstDays = range(7 - lastDayOfMonth === 1 ? 0 : 7 - lastDayOfMonth - 1).map(index => {
+    const getCurrentMonthDays = range(numberOfDaysInCurrentMonth).map(index => {
+      return {
+        value: index + 1,
+        month: CURRENT,
+      };
+    });
+
+    //6 is Saturday then we don't need any days from next month, in other case we need 6 - lastDayOfMonth days from 1 to 6;
+    const getNextMonthFirstDays = range(lastDayOfMonth === 6 ? 0 : 6 - lastDayOfMonth).map(index => {
       return {
         value: index + 1,
         month: NEXT,
       };
     });
 
-    const allVisibleDays = [ ...getPrevMonthLastDays, ...range(numberOfDaysInCurrentMonth).map(index => {
-      return {
-        value: index + 1,
-        month: CURRENT,
-      };
-    }), ...getNextMonthFirstDays ];
-    return allVisibleDays;
-  }
+    return [ ...getPrevMonthLastDays, ...getCurrentMonthDays, ...getNextMonthFirstDays ];
+  };
 
   monthDays.value = calculateDays();
 
   const setNextMonth = () => {
-    date.value.getMonth() + 1 > 11 ?
-    date.value = new Date(date.value.getFullYear() + 1, 0) :
-    date.value = new Date(date.value.getFullYear(), date.value.getMonth() + 1);
+    const dateObject = {
+      year: date.value.getFullYear(),
+      month: date.value.getMonth() + 1,
+    };
+
+    //checking if next is January
+    if (dateObject.month > 11) {
+      dateObject.year++;
+      dateObject.month = 0;
+    }
+
+    date.value = new Date(dateObject.year, dateObject.month);
     monthDays.value = calculateDays();
   };
 
   const setPrevMonth = () => {
-    date.value.getMonth() - 1 < 0 ?
-    date.value = new Date(date.value.getFullYear() - 1, 11) :
-    date.value = new Date(date.value.getFullYear(), date.value.getMonth() - 1);
+    const dateObject = {
+      year: date.value.getFullYear(),
+      month: date.value.getMonth() - 1,
+    };
+
+    //checking if previous is December
+    if (dateObject.month < 0) {
+      dateObject.year--;
+      dateObject.month = 11;
+    }
+
+    date.value = new Date(dateObject.year, dateObject.month);
     monthDays.value = calculateDays();
   };
 
